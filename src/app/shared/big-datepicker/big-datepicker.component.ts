6e6f31c7592d55fed32b639057d490d2416b5340
Orlando <vhagar91@gmail.com>
2018-12-14 10:30:45 -0500

    Big datepiker

--- src/app/shared/big-datepicker/big-datepicker.component.ts
+++ src/app/shared/big-datepicker/big-datepicker.component.ts
@@ -1,9 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'zerofee-app-big-datepicker',
   templateUrl: './big-datepicker.component.html',
-  styleUrls: ['./big-datepicker.component.scss']
+  styleUrls: ['./big-datepicker.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BigDatepickerComponent implements OnInit {
   @Input()
